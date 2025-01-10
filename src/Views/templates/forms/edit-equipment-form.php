324c06bea0fd23a58ec314a9d5fdb6e1cf1fd42d
4ndrisw <4ndr1sw@gmail.com>
2025-01-11 02:43:46 +0700

    Update edit-equipment-form.php

--- src/Views/templates/forms/edit-equipment-form.php
+++ src/Views/templates/forms/edit-equipment-form.php
@@ -70,7 +70,7 @@ defined('ABSPATH') || exit;
                         <select name="user_id" id="edit-user" class="regular-text">
                             <option value=""><?php _e('Pilih Admin', 'wp-equipment'); ?></option>
                             <?php
-                            $users = get_users(['role__in' => ['Equipment']]);
+                            $users = get_users(['role__in' => ['Customer']]);
                             foreach ($users as $user) {
                                 printf(
                                     '<option value="%d">%s</option>',
