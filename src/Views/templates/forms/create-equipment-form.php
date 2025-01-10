be58194f437e5d364e2b4a3ca9b9161f535030b7
4ndrisw <4ndr1sw@gmail.com>
2025-01-11 02:43:42 +0700

    Update create-equipment-form.php

--- src/Views/templates/forms/create-equipment-form.php
+++ src/Views/templates/forms/create-equipment-form.php
@@ -80,7 +80,7 @@ defined('ABSPATH') || exit;
                         <option value=""><?php _e('Pilih Admin', 'wp-equipment'); ?></option>
                         <?php
                         //$users = get_users(['role__in' => ['administrator', 'editor', 'author']]);
-                        $users = get_users(['role__in' => ['Equipment']]);
+                        $users = get_users(['role__in' => ['Customer']]);
                         foreach ($users as $user) {
                             printf(
                                 '<option value="%d">%s</option>',
